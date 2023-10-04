9876f71b2e4e5fc5e375509ab3a93b3960cacdc5
borja <msndeborjabolado@gmail.com>
2023-10-04 20:42:02 +0200

    block3 casi terminado

--- block3/Competition.php
+++ block3/Competition.php
@@ -15,32 +15,76 @@ class Competition {
         $this->arrayasociativo+=[$r->getcode() => $r];
     }
 
-    public function mostrarCorredor(){
+    public function mostrarCorredores(){
 
         print_r($this->arrayasociativo);
         
     }
 
 
-    public function addToRunner($c , $t){
+    public function addRaceToRunner($idCorredor , $tiempo){
         
-        $runner = $this->arrayasociativo[$c];
+        $idrunner = $this->arrayasociativo[$idCorredor];
         try{
-            $runner->addRace();
-        }catch{
-            
+            $idrunner->addRace($tiempo);
+        }catch(Exception $e){
+            echo"add Race salio mal";
+            return $e->getMessage();
+        }
+    }
+
+    public function calculate_The_Avg(){
+        $avg=0;
+        $tiempototal=0;
+        $contador =0;
+        foreach($this->arrayasociativo as $key=>$value){
+            $tiempototal += $value->gettiempos()[0];
+            $contador++;
+        }
+        $avg = $tiempototal/$contador;
+        //puedo hacer un size of del arrayasociativo y quitar el contador
+        return $avg;
+    }   
+
+
+
+    public function RunnerQuiquestRace(){
+        $codigo;
+        $carreraMasRapida=100000;
+        foreach($this->arrayasociativo as $key=>$value){
+            foreach($value->gettiempos() as $clave=> $valor){
+                if($valor<$carreraMasRapida ){
+                    $carreraMasRapida = $valor;
+                    $codigo = $value->getname();
+                }
+
+            }
+        }
+        return $codigo;
+    }
+
+
+    public function moreThan2WithMore15(){
+        $array = array();
+       
+        foreach($this->arrayasociativo as $key=>$value){
+            $cont =0;
+            foreach($value->gettiempos() as $clave=>$valor){
+                if($valor>15){
+                   $cont++;
+                }
+            }
+            if($cont>2){
+                array_push($array, $value->getname());
+            }
+            return $array;
+
         }
 
 
-
-       
     }
 
-
-
-    }
-
-
+}
 ?>
 
 
--- block3/Prueba.php
+++ block3/Prueba.php
@@ -10,15 +10,29 @@ $r3 = new Runner("iñako", "f43tgg6");
 
 $competicion1 = new Competition();
 
-$r1->addRace(12);
-$r1->addRace(3);
+$r1->addRace(16);
+$r1->addRace(45);
+$r1->addRace(26);
 $r2->addRace(11);
 $r3->addRace(4);
 
 $competicion1->addRunner($r1);
 $competicion1->addRunner($r2);
 $competicion1->addRunner($r3);
-$competicion1->mostrarCorredor();
+$competicion1->mostrarCorredores();
+
+$competicion1->addRaceToRunner("24f4g4",30);
+$competicion1->addRaceToRunner("f43tg56",50);
+
+$competicion1->mostrarCorredores();
+
+echo "La media es ".$competicion1->calculate_The_Avg();
+echo "<br>";
+echo "La carrera mas rapida es " . $competicion1->RunnerQuiquestRace();
+
+$array15 = $competicion1->moreThan2WithMore15();
+print_r($array15);
+
 
 
 ?>
